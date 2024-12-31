c22b468a2efa89fa6ff1c88ec05e40ef51072951
Susan Shrestha <susanshrestha783@gmail.com>
2024-12-31 19:16:52 +0545

    repl env

--- src/main.cs
+++ src/main.cs
@@ -2,9 +2,12 @@ using System.Net;
 using System.Net.Sockets;
 
 // Uncomment this line to pass the first stage
-Console.Write("$ ");
+while (true)
+{
+    Console.Write("$ ");
 
-// Wait for user input
-var command = Console.ReadLine();
+    // Wait for user input
+    var command = Console.ReadLine();
 
-Console.WriteLine($"{command}: command not found");
+    Console.WriteLine($"{command}: command not found");
+}
