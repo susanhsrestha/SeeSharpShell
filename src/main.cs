df2cb524639245713f1e64dc96762e119b487dd4
Susan Shrestha <susanshrestha783@gmail.com>
2024-12-31 19:12:03 +0545

    invalid argument in shell

--- src/main.cs
+++ src/main.cs
@@ -5,4 +5,6 @@ using System.Net.Sockets;
 Console.Write("$ ");
 
 // Wait for user input
-Console.ReadLine();
+var command = Console.ReadLine();
+
+Console.WriteLine($"{command}: command not found");
