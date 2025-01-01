3d31261464e90351024e3614a0e57fc0d223d929
Susan Shrestha <susanshrestha783@gmail.com>
2025-01-01 13:22:39 +0545

    run a program via the shell

--- src/main.cs
+++ src/main.cs
@@ -1,24 +1,62 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Collections;
+using System.Diagnostics;
 
+string[] builtinCommands = { "exit", "echo", "type" };
 // Uncomment this line to pass the first stage
-while (true)
+var getPathVariables = Environment.GetEnvironmentVariable("PATH");
+var delimiter = ":";
+// check if the system is windows and set the delimiter to ';'
+if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+    delimiter = ";";
+var pathVariables = getPathVariables.Split(':');
+
+bool run = true;
+
+while (run)
 {
     Console.Write("$ ");
 
     // Wait for user input
     string command = Console.ReadLine();
     string[] commandParse = command.Split(' ');
-    string[] builtinCommands = { "exit", "echo", "type" };
-    var getPathVariable = Environment.GetEnvironmentVariable("PATH");
-    var pathVariables = getPathVariable.Split(':');
+    CommandType commandType = GetCommandType(commandParse[0]);
+    if (commandType == CommandType.BuiltinCommand)
+    {
+        ExecuteBuiltinCommands(commandParse);
+    }
+    else if (commandType == CommandType.ExternalCommand)
+    {
+        ExecuteExternalCommands(commandParse);
+    }
+    else
+    {
+        Console.WriteLine($"{command}: not found");
+    }
+}
 
+void ExecuteExternalCommands(string[] commandParse)
+{
+    var exec = GetExecutible(commandParse[0]);
+    var Process = new Process();
+    Process.StartInfo.FileName = exec;
+    Process.StartInfo.Arguments = string.Join(" ", commandParse[1..]);
+    Process.Start();
+    Process.WaitForExit();
+    return;
+}
+
+void ExecuteBuiltinCommands(string[] commandParse)
+{
     switch (commandParse[0])
     {
         case "exit":
             if (commandParse.Length > 1 && commandParse[1] == "0")
+            {
+                run = false;
                 return;
+            }
             Console.WriteLine($"{commandParse[0]}: command needs more argument to exit");
             break;
 
@@ -32,33 +70,44 @@ while (true)
         case "type":
             if (commandParse.Length > 1)
             {
-                bool isFound = false;
-                if (builtinCommands.Contains(commandParse[1]))
+                var CommandType = GetCommandType(commandParse[1]);
+                switch (CommandType)
                 {
-                    Console.WriteLine($"{commandParse[1]} is a shell builtin");
-                    isFound = true;
-                    break;
-                }
-                else
-                {
-                    foreach (var path in pathVariables)
-                    {
-                        string fullPath = Path.Combine(path, commandParse[1]);
-                        if (File.Exists(fullPath))
-                        {
-                            Console.WriteLine($"{commandParse[1]} is {fullPath}");
-                            isFound = true;
-                            break;
-                        }
-                    }
-                    if (!isFound)
+                    case CommandType.BuiltinCommand:
+                        Console.WriteLine($"{commandParse[1]} is a shell builtin");
+                        break;
+                    case CommandType.ExternalCommand:
+                        Console.WriteLine($"{commandParse[1]} is {GetExecutible(commandParse[1])}");
+                        break;
+                    case CommandType.NotFound:
                         Console.WriteLine($"{commandParse[1]}: not found");
+                        break;
                 }
+                break;
             }
-            break;
-
-        default:
-            Console.WriteLine($"{commandParse[0]}: command not found");
+            Console.WriteLine($"{commandParse[0]}: command needs more argument to execute");
             break;
     }
 }
+
+CommandType GetCommandType(string command)
+{
+    if (builtinCommands.Contains(command))
+        return CommandType.BuiltinCommand;
+    else if (GetExecutible(command) != null)
+        return CommandType.ExternalCommand;
+    else return CommandType.NotFound;
+}
+
+string? GetExecutible(string command)
+{
+    foreach (var path in pathVariables)
+    {
+        string fullPath = Path.Combine(path, command);
+        if (File.Exists(fullPath))
+            return fullPath;
+    }
+    return null;
+}
+
+enum CommandType { NotFound, BuiltinCommand, ExternalCommand }
